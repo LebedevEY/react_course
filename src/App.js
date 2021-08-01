942df575be5491b68218de3fda4ac1fe7e05b452
Evgeniy Lebedev <evilhamster@yandex.ru>
2021-08-01 22:21:01 +0300

    01/08

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
-function App() {
+export function App() {
+    const [messageList, setMessageList] = useState([]);
+    const [value, setValue] = useState("")
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (messageList.length !== 0 && messageList[messageList.length - 1].author === "User") {
+                setMessageList(state => [...state, {value: "Message delivered!", author: "Bot"}]);
+            }
+        }, 1500);
+    }, [messageList]);
+
+
     return (
         <div className="App">
+            <ul className="message_list">
+                {messageList.map(message => <li><span>{message.author}</span>: {message.value}</li>)}
+            </ul>
+            <div className="input_form">
+                <input type="text" value={value} onChange={(event) => setValue(event.target.value)}/>
+                <button onClick={() => {
+                    setMessageList(state => [...state, {value, author: "User"}]);
+                    setValue("");
+                }}>Send
+                </button>
+            </div>
         </div>
     );
 }
-
-export default App;
