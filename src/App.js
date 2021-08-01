41935fc7bf2a8344285a45a944d1d2dfe79e1038
Evgeniy Lebedev <evilhamster@yandex.ru>
2021-08-01 13:59:12 +0300

    01/08

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 
-function App(props) {
+function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>{props.message}</h1>
-            </header>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-function sayHello (myMessage) {
-    const message = myMessage;
-    return message;
-}
+
 
 ReactDOM.render(
     <React.StrictMode>
-        <App message={sayHello('Hello GB!')} />
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
